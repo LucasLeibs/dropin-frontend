c41272ec709026bd2d13b3674b3722feffbfd46b
LucasLeibs <dr.lucasleibs@gmail.com>
2020-10-18 23:21:01 -0700

    working on delete comment

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -30,7 +30,13 @@ class Comments extends React.Component {
           this.setState ({[e.target.name]: e.target.value})
 
       }
-
+      deleteComment= event => {
+         if (event.user.id === this.props.user.id) {
+            
+         }
+          console.log(event)
+        //   if (this.props.user.id === id
+      }
 
    render() {
        console.log(this.props.event)
@@ -38,9 +44,13 @@ class Comments extends React.Component {
     <path fill-rule="evenodd" d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
     <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
   </svg>
-  const trash = <svg className="trash" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  const trash = <svg  width="1em" height="1em" viewBox="0 0 16 16" class="bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
   <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+</svg>
+const send =<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-right-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+<path fill-rule="evenodd" d="M4 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5A.5.5 0 0 0 4 8z"/>
 </svg>
        return(
            <div> 
@@ -48,17 +58,20 @@ class Comments extends React.Component {
             {this.props.event.comments.map(comment => (
            
            <div className="comment">
-           <p className="comment-name">{comment.user.first_name} </p>
-           <p className="comment-body">"{comment.body}"{trash}</p>
+           <p className="comment-name">{comment.user.first_name} {comment.user.last_name} </p>
+           <p className="comment-body">"{comment.body}"{<svg onClick={()=> this.deleteComment(comment)} id={comment.id} width="1em" height="1em" viewBox="0 0 16 16" class="bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+</svg>}</p>
            
-           <p className="comment-time">{moment(comment.created_at).startOf('day').fromNow()}</p>
+           <p className="comment-time">{moment(comment.created_at).startOf('hour').fromNow()}</p>
            </div>
        ))}
         <form onSubmit={this.handleSubmit} class="form-inline">
   <div class="form-group mx-sm-3 mb-2">
-    <input type="text" name="newComment" value={this.state.newComment} class="form-control" id="inputPassword2" placeholder="New Comment" onChange={this.handleCommentChange}></input>
+    <input  className="new-comment-form" type="text" name="newComment" value={this.state.newComment} class="form-control" id="inputPassword2" placeholder="New Comment" onChange={this.handleCommentChange}></input>
   </div>
-  <button type="submit" class="btn btn-primary mb-2">{quote}</button>
+  <button type="submit" class="btn btn-primary mb-2">{send}</button>
 </form>
     
     
--- src/components/EventShowPage.js
+++ src/components/EventShowPage.js
@@ -100,9 +100,7 @@ class EventShowPage extends React.Component {
       <path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
       <path fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
     </svg>
-    const star = <svg onClick={()=> this.saveEvent()} width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-star" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    <path fill-rule="evenodd" d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
-  </svg>
+    
     const pin = <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-geo" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
     <path fill-rule="evenodd" d="M8 1a3 3 0 1 0 0 6 3 3 0 0 0 0-6zM4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.319 1.319 0 0 0-.37.265.301.301 0 0 0-.057.09V14l.002.008a.147.147 0 0 0 .016.033.617.617 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.619.619 0 0 0 .146-.15.148.148 0 0 0 .015-.033L12 14v-.004a.301.301 0 0 0-.057-.09 1.318 1.318 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465-1.281 0-2.462-.172-3.34-.465-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411z"/>
   </svg>
@@ -120,7 +118,7 @@ class EventShowPage extends React.Component {
                 <div className="wrapper">
         <header className="header"><h1 className="event-h1">{name}
         {this.state.joined === true ? <button className="event-button" onClick={()=> this.joinEvent()}>Joined {check} </button> : <button className="event-button" onClick={()=> this.joinEvent()}>Join Event</button> }
-        {star}</h1>
+        </h1>
         <Alert color="danger" isOpen={this.state.visible} toggle={this.onDismiss}>
       You are already attending this event!
     </Alert>
@@ -138,7 +136,7 @@ class EventShowPage extends React.Component {
     <h6 className="event-p"></h6>
     <p className="event-p">{details}</p>
     <div className="comments">
-    <button onClick={() => this.loadComments(this.props.event.id)}>View Comments({this.props.event.comments.length})</button>
+    <button  className="view-comments" onClick={() => this.loadComments(this.props.event.id)}>View Comments({this.props.event.comments.length})</button>
     <Collapse isOpen={this.state.commentsOpen}>
    <Comments user = {this.props.user} event={this.props.event} comments ={this.state.comments}/>
 
