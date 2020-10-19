b3e6329ae7e843f7b21fe0b57bc0d8a001084ea5
LucasLeibs <dr.lucasleibs@gmail.com>
2020-10-18 18:32:54 -0700

    pushing so i dont lose evrything, working on comment funcionality

---
+++ src/components/Comments.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import moment from 'moment'
+import {InputGroup, InputGroupAddon, Input} from 'reactstrap'
+import 'reactstrap'
+
+class Comments extends React.Component {
+    state = {
+        
+    
+    }
+
+    newCommentForm = (e) => {
+        console.log("hi")
+        this.setState({
+          newCommentForm: !this.state.newCommentForm
+        })
+        
+      }
+
+
+   render() {
+    const quote = <svg  width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-chat-dots" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
+    <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+  </svg>
+  const trash = <svg className="trash" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+</svg>
+       return(
+           <div> 
+              
+            {this.props.comments.map(comment => (
+           
+           <div className="comment">
+           <p className="comment-name">{comment.user.first_name} </p>
+           <p className="comment-body">"{comment.body}"{trash}</p>
+           
+           <p className="comment-time">{moment(comment.created_at).startOf('day').fromNow()}</p>
+           </div>
+       ))}
+    
+     
+    <InputGroup size="sm">
+        <Input type="text" name="newComment" value={this.state.newComment} placeholder="New Comment"/>
+        <InputGroupAddon addonType="append" type="submit">
+           {quote}
+        </InputGroupAddon>
+      </InputGroup>
+       </div>
+
+       )
+   }
+}
+
+export default Comments
--- src/components/EventShowPage.js
+++ src/components/EventShowPage.js
@@ -6,6 +6,7 @@ import UserAvatar from 'react-user-avatar'
 import { Alert } from 'reactstrap';
 import {Collapse} from 'reactstrap'
 import moment from 'moment'
+import Comment from './Comments.js'
 
 class EventShowPage extends React.Component {
     state= {
@@ -16,7 +17,6 @@ class EventShowPage extends React.Component {
       visible: false,
       comments: [],
       commentsOpen: false,
-      newCommentForm: false
     }
 
     loadComments = (id) => {
@@ -28,12 +28,7 @@ console.log(id)
         commentsOpen: !this.state.commentsOpen
       }))
     }
-    newCommentForm = (e) => {
-      this.setState({
-        newCommentForm: !this.state.newCommentForm
-      })
-      
-    }
+    
     newComment= (e) => {
       axios.post('http://localhost:3000/comments', {withCredentials: true})
     }
@@ -85,15 +80,9 @@ console.log(id)
             if(ids.includes(this.props.event.id)) {
               this.setState({
                 attending_events: user[0].attending_events,
-                joined: true
+                joined: !this.state.joined
               })
-            } else {
-              this.setState({
-                joined: false,
-                attending_events: user[0].attending_events
-              })
-            }
-    
+            } 
         })
     }
     render () {
@@ -127,10 +116,7 @@ console.log(id)
         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
       </svg>
-       const quote = <svg  width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-chat-dots" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-       <path fill-rule="evenodd" d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
-       <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-     </svg>
+       
        return (
             <div className="show-page">
                 <div className="wrapper">
@@ -156,18 +142,9 @@ console.log(id)
     <div className="comments">
     <button onClick={() => this.loadComments(this.props.event.id)}>comments</button>
     <Collapse isOpen={this.state.commentsOpen}>
-    <p onClick={() => this.newCommentForm()} className="comment-body">{quote}New Comment</p>
-    {this.state.comments.map(comment => (
-           
-            <div className="comment">
-            <p className="comment-name">{comment.user.first_name} </p>
-            <p className="comment-body">{comment.body}{trash}</p>
-            
-            <p className="comment-time">{moment(comment.created_at).startOf('day').fromNow()}</p>
-            
-           
-            </div>
-        ))}
+   <Comment user = {this.props.user} event={this.props.event} comments ={this.state.comments}/>
+   
+   
     </Collapse>
   </div>
     </div>
